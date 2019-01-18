ce6d193be4db483017d7b1bb65cc378f2c508d8f
Rod Glover <rglover@uvic.ca>
2019-01-18 09:58:09 -0800

    Rebuild package

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 // TODO: Remove this dummy export (left over from initial testing)
 export { default as TextInput } from './components/dummy/TextInput/TextInput'; // Selector components
 
-export { default as DatasetSelector } from './components/select/DatasetSelector';
+export { default as DatasetSelector } from './components/select/DataspecSelector';
 export { default as EmissionsScenarioSelector } from './components/select/EmissionsScenarioSelector';
 export { default as GroupingSelector } from './components/select/GroupingSelector';
 export { default as ModelSelector } from './components/select/ModelSelector';
