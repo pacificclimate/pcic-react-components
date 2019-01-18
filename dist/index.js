27126a6b07966cac9817ffb4d9e901c0ab15ce71
Rod Glover <rglover@uvic.ca>
2019-01-18 13:22:55 -0800

    Rebuild package

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,5 @@
 // Selector components
-export { default as DatasetSelector } from './components/select/DataspecSelector';
+export { default as DataspecSelector } from './components/select/DataspecSelector';
 export { default as EmissionsScenarioSelector } from './components/select/EmissionsScenarioSelector';
 export { default as GroupingSelector } from './components/select/GroupingSelector';
 export { default as ModelSelector } from './components/select/ModelSelector';
