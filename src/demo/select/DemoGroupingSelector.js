e86152dfa7be262444eae0c6fdfe39b722c7a32f
Rod Glover <rglover@uvic.ca>
2019-03-26 17:51:02 -0700

    Extend DemoGroupingSelector

--- src/demo/select/DemoGroupingSelector.js
+++ src/demo/select/DemoGroupingSelector.js
@@ -3,15 +3,32 @@ import { Grid, Row, Col, Button, Glyphicon } from 'react-bootstrap';
 import GroupingSelector from '../../lib/components/select/GroupingSelector';
 import ModelSelector from '../../lib/components/select/ModelSelector';
 import meta from './assets/meta';
+import Select from 'react-select';
+import VariableSelector from '../../lib/components/select/VariableSelector';
+import EmissionsScenarioSelector from '../../lib/components/select/EmissionsScenarioSelector';
+import DataspecSelector from '../../lib/components/select/DataspecSelector';
 
 function stringify(obj) {
   return <pre>{JSON.stringify(obj, null, 2)}</pre>;
 }
 
+const selectors = [
+  { label: 'Model Selector', value: ModelSelector, foo: 'bar' },
+  { label: 'Emissions Scenario Selector', value: EmissionsScenarioSelector },
+  { label: 'Variable Selector', value: VariableSelector },
+  { label: 'Dataspec Selector', value: DataspecSelector },
+];
+
+
 export default class DemoGroupingSelector extends Component {
   state = {
+    selector: selectors[1],
     value: null,
   };
+
+  handleChangeSelector = selector => this.setState(
+    { selector, value: null }
+  );
 
   handleChangeSelectorValue = (value) =>
     this.setState({ value });
@@ -21,9 +38,20 @@ export default class DemoGroupingSelector extends Component {
       <Grid fluid>
         <Row>
           <Col lg={3}>
+            <Select
+              options={selectors}
+              value={this.state.selector}
+              onChange={this.handleChangeSelector}
+            />
+          </Col>
+          <Col lg={3}>
+            Selector:
+            {stringify(this.state.selector)}
+          </Col>
+          <Col lg={3}>
             <GroupingSelector
               bases={meta}
-              getOptionRepresentative={ModelSelector.getOptionValue}
+              getOptionRepresentative={this.state.selector.value.getOptionValue}
               value={this.state.value}
               onChange={this.handleChangeSelectorValue}
               debug={true}
